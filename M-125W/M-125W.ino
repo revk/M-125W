21497d3ec0bfd886d1b9e019c6bdefdfc393781a
Adrian Kennard <adrian@158.176.240.91.in-addr.arpa>
2019-04-16 15:35:32 +0100

    Using ESP8266RevK library now

--- M-125W/M-125W.ino
+++ M-125W/M-125W.ino
@@ -1,18 +1,30 @@
 // Marsden Scales
 // Reporting via MQTT
 
-#include "revk.h"
+#include "info.h"
+#include <ESP8266RevK.h>
+
+void app_wrap(char*topic, uint8_t*message, unsigned int len);
+void app_mqtt(const char *prefix, const char*suffix, const byte *message, size_t len);
+ESP8266RevK revk("M-125W",app_wrap,app_mqtt);
+
+void app_wrap(char*topic, uint8_t*message, unsigned int len)
+{
+  revk.message(topic,message,len);
+}
 
 void app_mqtt(const char *prefix, const char*suffix, const byte *message, size_t len)
 {
   if (!suffix)return;
-  if (!strcmp(prefix, "cmnd") && !strcmp(suffix, "send"))
+  if(strcmp(prefix, "cmnd"))return;
+  if (!strcmp(suffix, "send"))
   {
+    revk.pub("error", suffix, "Can't do send button yet");
     // TODO press SEND button
-  }
+  } else revk.pub("error", suffix, "Unknown command");
 }
 
-void app_setup()
+void setup()
 {
   Serial.begin(9600); // Marsden talks at 9600 Baud
 }
@@ -21,8 +33,9 @@ void app_setup()
 char line[MAX_LINE + 1];
 int linep = 0;
 
-void app_loop()
+void loop()
 {
+  revk.loop();
   if (Serial.available() > 0) {
     char c = Serial.read();
     if (c >= ' ')
@@ -36,8 +49,7 @@ void app_loop()
         if (!strncmp(line, "NET WEIGHT", 10))
         {
           char *p = line + 10;
-          while (*p == ' ')p++;
-          revk_pub("stat", "weight", p);
+          while (*p == ' ')p++;          revk.pub("stat", "weight", "%s", p);
         }
       }
       linep = 0;
--- M-125W/revk.h
+++
@@ -1,5 +0,0 @@
-// Functios and data provided to the app
-
-void revk_pub(const char *prefix,const char *suffix,const char *fmt,...);
-
-extern char chipid[7];
