985d1062ab6a4135cc810f02fb6811dad8cdb568
RevK <github@e.gg>
2019-04-27 13:46:41 +0100

    New RevK library

--- M-125W/M-125W.ino
+++ M-125W/M-125W.ino
@@ -32,9 +32,14 @@ ESP8266RevK revk(__FILE__, "Build: " __DATE__ " " __TIME__);
 #define USE_SPI
 #endif
 
-// My settings
-char cloudhost[129] = "weigh.me.uk";
-char cloudpass[33] = "";
+#define app_settings \
+  s(cloudhost); \
+  s(cloudpass); \
+
+#define s(n) const char *n=NULL
+  app_settings
+#undef s
+
 
 #define SEND  1 // Send button (TX pin)
 #define RST 2 // SPI
@@ -43,189 +48,183 @@ char cloudpass[33] = "";
 #ifdef USE_SPI
 #include <SPI.h>
 #include <MFRC522.h>
-MFRC522 rfid(SS, RST); // Instance of the class
+  MFRC522 rfid(SS, RST); // Instance of the class
 #endif
 
-void pressend();
+  void pressend();
 
-boolean app_setting(const char *setting, const byte *value, size_t len)
-{ // Called for settings retrieved from EEPROM
-  if (!strcasecmp(setting, "cloudhost") && len < sizeof(cloudhost)) {
-    memcpy(cloudhost, value, len);
-    cloudhost[len] = 0;
-  } else if (!strcasecmp(setting, "cloudpass") && len < sizeof(cloudpass)) {
-    memcpy(cloudpass, value, len);
-    cloudpass[len] = 0;
+  const char * app_setting(const char *tag, const byte *value, size_t len)
+  { // Called for settings retrieved from EEPROM
+#define s(n) do{const char *t=PSTR(#n);if(!strcmp_P(tag,t)){n=(const char *)value;return t;}}while(0)
+    app_settings
+#undef s
+    return NULL; // Done
   }
-  else
-    return false; // Unknown setting
-  return true; // Done
-}
 
-boolean app_cmnd(const char*suffix, const byte *message, size_t len)
-{ // Called for incoming MQTT messages
-  if (!strcasecmp(suffix, "send"))
+  boolean app_command(const char*tag, const byte *message, size_t len)
+  { // Called for incoming MQTT messages
+    if (!strcasecmp_P(tag, PSTR("send")))
+    {
+      presssend();
+      return true;
+    }
+    return false;
+  }
+
+  void setup()
   {
-    presssend();
-    return true;
-  }
-  return false;
-}
-
-void setup()
-{
 #ifdef REVKDEBUG
-  Serial.println("Started " __FILE__);
+    Serial.println("Started " __FILE__);
 #else
-  Serial.begin(9600); // Marsden talks at 9600 Baud
-  digitalWrite(SEND, HIGH);
-  pinMode(SEND, INPUT);
+    Serial.begin(9600); // Marsden talks at 9600 Baud
+    digitalWrite(SEND, HIGH);
+    pinMode(SEND, INPUT);
 #endif
 #ifdef USE_SPI
-  SPI.begin(); // Init SPI bus
-  rfid.PCD_Init(); // Init MFRC522
+    SPI.begin(); // Init SPI bus
+    rfid.PCD_Init(); // Init MFRC522
 #endif
-}
+  }
 
 #define MAX_LINE 100
-char line[MAX_LINE + 1];
-int linep = 0;
+  char line[MAX_LINE + 1];
+  int linep = 0;
 
-unsigned sendbutton = 0; // Signed to allow for wrap
-void presssend()
-{
-  if (!(sendbutton = millis() + 250))sendbutton++; // Dont allow 0 to happen
-#ifdef REVKDEBUG
-  Serial.println("Send low");
-#else
-  digitalWrite(SEND, LOW);
-  pinMode(SEND, OUTPUT);
-#endif
-}
-
-int report(byte *id, char *weight)
-{
-  if (!*cloudpass)
-  { // We need a password - make one - store in flash - so we use same every time
-    int i;
-    for (i = 0; i < sizeof(cloudpass) - 1; i++)cloudpass[i] = 'A' + ESP8266TrueRandom.random(26);
-    cloudpass[i] = 0; // End
-    revk.setting("cloudpass", cloudpass); // save
-  }
-  if (weight && id)
-    revk.pub("stat", "idweight", "%02X%02X%02X%02X %s", id[0], id[1], id[2], id[3], weight);
-  else if (weight)
-    revk.pub("stat", "weight", "%s", weight);
-
-  if (!*cloudhost)return 404;
-  // Post
-  char url[500];
-  int m = sizeof(url) - 1, p = 0;
-  if (p < m)p += snprintf(url + p, m - p, "/weighin.cgi?version=%s", __DATE__ " " __TIME__);
-  if (p < m)p += snprintf(url + p, m - p, "&scales=%06X", ESP.getChipId());
-  if (p < m && cloudpass)p += snprintf(url + p, m - p, "&auth=%s", cloudpass); // Assume no special characters
-  if (p < m && weight)p += snprintf(url + p, m - p, "&weight=%s", weight);
-  if (p < m && id)p += snprintf(url + p, m - p, "&id=%02X%02X%02X%02X", id[0], id[1], id[2], id[3]);
-  url[p] = 0;
-  for (p = 0; url[p]; p++)if (url[p] == ' ')url[p] = '+';
-  // Note, always https
-  WiFiClientSecure client;
-  revk.clientTLS(client);
-  HTTPClient https;
-  https.begin(client, cloudhost, 443, url, true);
-  int ret = https.GET();
-  https.end();
-  if (ret == HTTP_CODE_UPGRADE_REQUIRED)revk.ota(); // Upgrade required: New firmware required
-  if (ret > 0 && ret != HTTP_CODE_OK && ret != HTTP_CODE_NO_CONTENT)
-    revk.error("https", "Failed %d from %s", ret, cloudhost);
-  else if (ret < 0)
+  unsigned sendbutton = 0; // Signed to allow for wrap
+  void presssend()
   {
-    char err[100];
-    client.getLastSSLError(err, sizeof(err));
-    revk.error("https", "Failed %s: %s from %s", https.errorToString(ret).c_str(), err,  cloudhost);
-  }
-  return ret;
-}
-
-void loop()
-{
-  revk.loop();
-  long now = millis();
-  if (sendbutton && (int) (sendbutton - now) < 0)
-  { // Send button done
-    sendbutton = 0;
+    if (!(sendbutton = millis() + 250))sendbutton++; // Dont allow 0 to happen
 #ifdef REVKDEBUG
-    Serial.println("Send high");
+    Serial.println("Send low");
 #else
-    pinMode(SEND, INPUT);
-    digitalWrite(SEND, HIGH);
+    digitalWrite(SEND, LOW);
+    pinMode(SEND, OUTPUT);
 #endif
   }
-  static long sendretry = 0;
-  static long carddone = 0;
-  static byte cardid[4] = {};
-  if (carddone && (int)(carddone - now) < 0)
-  { // Card read timed out
-    report(cardid, NULL);
-    carddone = 0;
-    sendretry = 0;
-  }
-  if (sendretry && (int)(sendretry - now) < 0)
+
+  int report(byte *id, char *weight)
   {
-    presssend();
-    if (!(sendretry = now + SENDRETRY))sendretry++;
-  }
-  while (Serial.available() > 0)
-  { // Get serial
-    char c = Serial.read();
-    if (c >= ' ')
-    { // line
-      if (linep < MAX_LINE)line[linep++] = c;
-    } else if (linep)
-    { // end of line
-      line[linep] = 0;
-      linep = 0; // new line
-      if (!strncmp(line, "NET WEIGHT", 10))
-      { // Time to send
-        char *p = line + 10;
-        while (*p == ' ')p++;
-        if (*p != '0')
-        { // Expect some weight - i.e. >=1 kg >=1 stone >=1 lb
-          if (carddone)report(cardid, p);
-          else report(NULL, p);
-          carddone = 0;
-          sendretry = 0;
-        }
-      }
+    if (!cloudpass)
+    { // We need a password - make one - store in flash - so we use same every time
+      int i;
+      char pass[33];
+      for (i = 0; i <  sizeof(pass) - 1; i++)pass[i] = 'A' + ESP8266TrueRandom.random(26);
+      pass[i] = 0; // End
+      revk.setting(F("cloudpass"), pass); // save
     }
-  }
-#ifdef USE_SPI
-  static long cardcheck = 0;
-  if ((int)(cardcheck - now) < 0)
-  {
-    cardcheck = now + 10;
-    if (rfid.PICC_IsNewCardPresent())
+    if (weight && id)
+      revk.event(F("idweight"), F("%02X%02X%02X%02X %s"), id[0], id[1], id[2], id[3], weight);
+    else if (weight)
+      revk.event(F("weight"), F("%s"), weight);
+    if (!cloudhost)cloudhost = "weigh.me.uk";
+    // Post
+    char url[500];
+    int m = sizeof(url) - 1, p = 0;
+    if (p < m)p += snprintf_P(url + p, m - p, PSTR("/weighin.cgi?version=%s"), __DATE__ " " __TIME__);
+    if (p < m)p += snprintf_P(url + p, m - p, PSTR("&scales=%06X"), ESP.getChipId());
+    if (p < m && cloudpass)p += snprintf_P(url + p, m - p, PSTR("&auth=%s"), cloudpass); // Assume no special characters
+    if (p < m && weight)p += snprintf_P(url + p, m - p, PSTR("&weight=%s"), weight);
+    if (p < m && id)p += snprintf_P(url + p, m - p, PSTR("&id=%02X%02X%02X%02X"), id[0], id[1], id[2], id[3]);
+    url[p] = 0;
+    for (p = 0; url[p]; p++)if (url[p] == ' ')url[p] = '+';
+    // Note, always https
+    WiFiClientSecure client;
+    revk.clientTLS(client);
+    HTTPClient https;
+    https.begin(client, cloudhost, 443, url, true);
+    int ret = https.GET();
+    https.end();
+    if (ret == HTTP_CODE_UPGRADE_REQUIRED)revk.ota(); // Upgrade required: New firmware required
+    if (ret > 0 && ret != HTTP_CODE_OK && ret != HTTP_CODE_NO_CONTENT)
+      revk.error(F("https"), F("Failed %d from %s"), ret, cloudhost);
+    else if (ret < 0)
     {
-      if (rfid.PICC_ReadCardSerial())
-      {
+      char err[100];
+      client.getLastSSLError(err, sizeof(err));
+      revk.error(F("https"), F("Failed %s: %s from %s"), https.errorToString(ret).c_str(), err,  cloudhost);
+    }
+    return ret;
+  }
 
-        MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
-        if (!carddone || memcmp(cardid, rfid.uid.uidByte, 4))
-        {
-          if (carddone)    report(cardid, NULL); // Change of card, report previous
-          revk.pub("stat", "id", "%02X%02X%02X%02X", rfid.uid.uidByte[0], rfid.uid.uidByte[1], rfid.uid.uidByte[2], rfid.uid.uidByte[3]);
-
+  void loop()
+  {
+    revk.loop();
+    long now = millis();
+    if (sendbutton && (int) (sendbutton - now) < 0)
+    { // Send button done
+      sendbutton = 0;
+#ifdef REVKDEBUG
+      Serial.println(F("Send high"));
+#else
+      pinMode(SEND, INPUT);
+      digitalWrite(SEND, HIGH);
+#endif
+    }
+    static long sendretry = 0;
+    static long carddone = 0;
+    static byte cardid[4] = {};
+    if (carddone && (int)(carddone - now) < 0)
+    { // Card read timed out
+      report(cardid, NULL);
+      carddone = 0;
+      sendretry = 0;
+    }
+    if (sendretry && (int)(sendretry - now) < 0)
+    {
+      presssend();
+      if (!(sendretry = now + SENDRETRY))sendretry++;
+    }
+    while (Serial.available() > 0)
+    { // Get serial
+      char c = Serial.read();
+      if (c >= ' ')
+      { // line
+        if (linep < MAX_LINE)line[linep++] = c;
+      } else if (linep)
+      { // end of line
+        line[linep] = 0;
+        linep = 0; // new line
+        if (!strncmp_P(line, PSTR("NET WEIGHT"), 10))
+        { // Time to send
+          char *p = line + 10;
+          while (*p == ' ')p++;
+          if (*p != '0')
+          { // Expect some weight - i.e. >=1 kg >=1 stone >=1 lb
+            if (carddone)report(cardid, p);
+            else report(NULL, p);
+            carddone = 0;
+            sendretry = 0;
+          }
         }
-        memcpy(cardid, rfid.uid.uidByte, 4);
-        if (!carddone)
-        {
-          presssend();
-          if (!(sendretry = now + SENDRETRY))
-            sendretry++;
-        }
-        if (!(carddone = now + CARDWAIT))carddone++;
       }
     }
-  }
+#ifdef USE_SPI
+    static long cardcheck = 0;
+    if ((int)(cardcheck - now) < 0)
+    {
+      cardcheck = now + 10;
+      if (rfid.PICC_IsNewCardPresent())
+      {
+        if (rfid.PICC_ReadCardSerial())
+        {
+
+          MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+          if (!carddone || memcmp(cardid, rfid.uid.uidByte, 4))
+          {
+            if (carddone)    report(cardid, NULL); // Change of card, report previous
+            revk.event(F("id"), F("%02X%02X%02X%02X"), rfid.uid.uidByte[0], rfid.uid.uidByte[1], rfid.uid.uidByte[2], rfid.uid.uidByte[3]);
+
+          }
+          memcpy(cardid, rfid.uid.uidByte, 4);
+          if (!carddone)
+          {
+            presssend();
+            if (!(sendretry = now + SENDRETRY))
+              sendretry++;
+          }
+          if (!(carddone = now + CARDWAIT))carddone++;
+        }
+      }
+    }
 #endif
-}
+  }
